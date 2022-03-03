9b7aab6cf1293d58aa187911d688436826ea7da9
Andrea <48454747+Admojo@users.noreply.github.com>
2022-03-03 16:24:50 +0100

    start scenario (andrea) del 4 - implementert scenario 1 og 2 fra Kainat ( endret navn)

--- Scenario/Scenario2.java
+++ Scenario/Scenario2.java
@@ -1,6 +1,6 @@
 import no.hiof.framework.KeyBoardEventlister;
 
-public class Scenerio2 {
+public class Scenario2 {
      public static void main(String[] args) {
          KeyBoardEventlister keyBoardEventlister = new KeyBoardEventlister();
     }
